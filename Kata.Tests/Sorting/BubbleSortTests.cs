2806f54018e70e3ee942d134149b19ea07453dde
EMAKINA\msr <msr@emakina.com>
2021-07-04 12:03:11 +0200

    Added bubble sort with TDD

---
+++ Kata.Tests/Sorting/BubbleSortTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using KataConsole.Sorting;
+
+namespace Kata.Tests.Sorting
+{
+    public class BubbleSortTests
+    {
+
+        [Theory]
+        [InlineData(new int[] { 1, 10, 11, 15, 4, 6 })]
+        public void ElementsShouldBeSorted(int[] toSort)
+        {
+            //Arrange
+            var bubbleSort = new BubbleSort();
+            //Act
+            bubbleSort.Sort(ref toSort);
+
+            //Assert
+            toSort.Should().BeInAscendingOrder();
+        }
+    }
+}
---
+++ KataConsole/Sorting/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataConsole.Sorting
+{
+    public class BubbleSort
+    {
+        public void Sort(ref int[] toSort)
+        {
+            var isMoved = false;
+            do
+            {
+                isMoved = false;
+                for (int i = 0; i < toSort.Length - 1; i++)
+                {
+                    if (toSort[i] > toSort[i + 1])
+                    {
+                        var lowerValue = toSort[i + 1];
+                        toSort[i + 1] = toSort[i];
+                        toSort[i] = lowerValue;
+                        isMoved = true;
+                    }
+                }
+            } while (isMoved);
+        }
+    }
+}
