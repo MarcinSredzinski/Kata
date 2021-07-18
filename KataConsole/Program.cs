5774e170dbbde7d21dd405a2d472f7c942f126ca
MarcinSredzinski <msr@emakina.com>
2021-07-18 11:29:06 +0200

    Code cleanup

--- KataConsole/Program.cs
+++ KataConsole/Program.cs
@@ -5,8 +5,3 @@ int[] toSort = new int[] { 1, 6, 8, 9, 4, 3 };
 BubbleSortVisualization bsw = new BubbleSortVisualization(toSort);
 
 bsw.Sort(ref toSort);
-
-//Let's see. Still testing. And now it works. 
-bsw.Visualize(toSort, 0); //ToDo remove.
-                          //And another comment.
-                          //And another
